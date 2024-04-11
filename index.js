018f7e98c478957d9ecc5e0ebef1bf7dcb231a21
Orace EDJO <oracejuscard@gmail.com>
2024-04-11 12:57:36 +0200

    Fin du backend

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 
 const app = express();
 const port = 7878;
-const hostname = "192.168.0.103";
+const hostname = "192.168.0.107";
 
 // Configurer la connexion à la base de données MySQL
 const connection = mysql.createConnection({
@@ -36,71 +36,36 @@ server.listen(port, hostname, () => {
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function (ws, req) {
-  const userID = req.url.substr(1);
-
- /*  ws.on('message', message => {
-    console.log(message);
-    const datastring = message.toString();
-    if (datastring.charAt(0) === '{') {
-      const data = JSON.parse(datastring);
-      if (data.auth === 'chatapphdfgjd34534hjdfk' && data.cmd === 'send') {
-        const positionData = {
-          userID: userID,
-          longitude: data.longitude,
-          latitude: data.latitude
-        };
-        // Insérer la position dans la base de données
-        const query = 'INSERT INTO positions SET ?';
-        connection.query(query, positionData, (error, results, fields) => {
-          if (error) {
-            console.error('Error inserting position into MySQL:', error);
-            ws.send('send:error');
-          } else {
-            console.log('Position saved in MySQL');
-            ws.send('send:success');
-          }
-        });
-      } else {
-        console.log('Invalid authentication or command');
-        ws.send('send:error');
-      }
-    } else {
-      console.log('Non JSON type data');
-      ws.send('error');
-    }
-  });
- */
-
+  
   ws.on('message', message => {
-    console.log(message);
     try {
       // Analyse le message JSON
       const data = JSON.parse(message);
       
-      // Enregistre les données de localisation dans la base de données
-      const positionData = {
-        longitude: data.longitude,
-        latitude: data.latitude
-      };
-      const query = 'INSERT INTO positions SET ?';
-      connection.query(query, positionData, (error, results, fields) => {
+      // Extrayez les données de localisation
+      const { userId, longitude, latitude } = data;
+
+      // Mettre à jour la base de données
+      const query = 'UPDATE users SET positions = JSON_SET(positions, "$.longitude", ?, "$.latitude", ?) WHERE id = ?';
+      connection.query(query, [longitude, latitude, userId], (error, results, fields) => {
         if (error) {
-          console.error('Error inserting position into MySQL:', error);
+          console.error('Error updating user position in MySQL:', error);
         } else {
-          console.log('Position saved in MySQL');
+          console.log('User position updated in MySQL');
         }
       });
     } catch (error) {
       console.error('Error parsing JSON:', error);
-      ws.send('error');
     }
   });
   
   ws.on('close', function () {
-    console.log('User Disconnected: ' + userID);
+    console.log('User Disconnected');
   });
 
-  ws.send('connected');
+  ws.send('connected', function(){
+    console.log('User connected');
+  });
 });
 
 app.on('error', error => console.error(error));
