4a895f88f2da2757d64a5786ac02e8a35c26b3c7
Orace EDJO <oracejuscard@gmail.com>
2024-04-12 10:30:39 +0200

    Tout est top

--- databases/configDatabase.js
+++ databases/configDatabase.js
@@ -3,6 +3,6 @@ module.exports = {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'otrip'
+    database: process.env.DB_NAME || 'madb'
   };
   
